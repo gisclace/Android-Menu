a96f4d49a7e48242c5643ac4861608c2c14a570c
gisclace <gisclace@gmail.com>
2015-04-03 15:11:56 +0200

    V2

--- app/src/main/java/gisclace/android_menu/MainActivity.java
+++ app/src/main/java/gisclace/android_menu/MainActivity.java
@@ -20,25 +20,32 @@ public class MainActivity extends TabActivity {
         TabSpec m1 = tabHost.newTabSpec("Menu 1");
         // Titre
         m1.setIndicator("Menu 1");
-        Intent photosIntent = new Intent(this, Menu1.class);
-        m1.setContent(photosIntent);
+        Intent m1Intent = new Intent(this, Menu1.class);
+        m1.setContent(m1Intent);
 
         // Tab du second menu
         TabSpec m2 = tabHost.newTabSpec("Menu 2");
         m2.setIndicator("Menu 2");
-        Intent songsIntent = new Intent(this, Menu2.class);
-        m2.setContent(songsIntent);
+        Intent m2Intent = new Intent(this, Menu2.class);
+        m2.setContent(m2Intent);
 
         // Tab du troisiéme menu
         TabSpec m3 = tabHost.newTabSpec("Menu 3");
         m3.setIndicator("Menu 3");
-        Intent videosIntent = new Intent(this, Menu3.class);
-        m3.setContent(videosIntent);
+        Intent m3Intent = new Intent(this, Menu3.class);
+        m3.setContent(m3Intent);
 
-        // Adding all TabSpec to TabHost
+        // Tab du quatriéme menu
+        TabSpec m4 = tabHost.newTabSpec("Menu 4");
+        m4.setIndicator("Menu 4");
+        Intent m4Intent = new Intent(this, Menu4.class);
+        m4.setContent(m4Intent);
+
+        // TabSpec --> TabHost
         tabHost.addTab(m1); // Tab menu 1
         tabHost.addTab(m2); // Tab menu 2
         tabHost.addTab(m3); // Tab menu 3
+        tabHost.addTab(m4); // Tab menu 4
 
     }
 }
